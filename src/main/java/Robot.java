06891f3076038677e7dbd552391f5f18ae906e1c
Adelita Esparza <aesparza051@gmail.com>
2019-07-08 09:35:46 -0500

    added another comment for notes

--- src/main/java/Robot.java
+++ src/main/java/Robot.java
@@ -1,4 +1,5 @@
 public interface Robot {
+    // creating methods for all files and no need to use properties as this is an interface file not an abstract file.
     public void turnOn();
     public void turnOff();
 }
